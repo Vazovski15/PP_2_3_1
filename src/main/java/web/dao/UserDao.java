483132388dac9ddea0700d210bb874cb733e399b
Vazovski15 <vladchusov15@gmail.com>
2022-12-29 22:45:18 +0300

    Initial commit

---
+++ src/main/java/web/dao/UserDao.java
@@ -0,0 +1,17 @@
+package web.dao;
+
+import web.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+    User getUser(int id);
+
+    List<User> getAllUsers();
+
+    void edit(int id, User user);
+
+    void add(User user);
+
+    void delete(int id);
+}
